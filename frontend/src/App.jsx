06dd48687b7d50ed18f8243dbaccc3278e29e88c
Manickam11 <221401049@rajalakshmi.edu.in>
2025-02-24 17:23:25 +0530

    git commit "update the trial page for melvin"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import CollegeMap from './components/CollegeMap';
 import Home from './components/Home';
 import AvailabilityRooms from './components/AvailabilityRooms';
 import Header from './components/Header';
+import CollegeMap2 from './components/CollegeMap2';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<CollegeMap />} />
         <Route path="/availability" element={<AvailabilityRooms />} />
+        <Route path="/collegemap" element={<CollegeMap2 />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/CollegeMap2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CollegeMap2 = () => {
+  return (
+    <div>
+      <h2>This is second Page</h2>
+    </div>
+  );
+};
+export default CollegeMap2; 
